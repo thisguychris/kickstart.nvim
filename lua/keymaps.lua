aa8b97da90c75bd809b3722066570019fb6dcd3e
chris <532797+thisguychris@users.noreply.github.com>
2024-01-02 06:58:11 +0800

    update descriptions in keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,6 +3,7 @@
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- general enhancements
-vim.keymap.set("n", "<leader>q", ":q!<cr>", { desc = "close window" })
-vim.keymap.set("n", "<leader>z", ":update<cr>", { desc = "save" })
-vim.keymap.set("n", "<leader>o", ":only<cr>", { desc = "only" })
+vim.keymap.set("n", "<leader>q", ":q!<cr>", { desc = "Close Window" })
+vim.keymap.set("n", "<leader>z", ":update<cr>", { desc = "Save" })
+vim.keymap.set("n", "<leader>o", ":only<cr>", { desc = "Only" })
+vim.keymap.set("n", "<leader>gg", ":Git<cr>", { desc = "Git Status" })
