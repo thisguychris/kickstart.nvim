ddabade76d0310f9c1a427dc260e76e40e5b8339
chris <532797+thisguychris@users.noreply.github.com>
2024-01-02 07:37:40 +0800

    update cursor to block

--- lua/enhancements.lua
+++ lua/enhancements.lua
@@ -1,1 +1,1 @@
-vim.o.guicursor = 'n-v-c-sm-i-ci-ve:ver25,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor'
+vim.o.guicursor = 'n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor'
