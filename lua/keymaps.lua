f1cf7a406c65bdbe77067f9e7fa027e72cea8247
chris <532797+thisguychris@users.noreply.github.com>
2024-01-02 18:03:09 +0800

    add silent in keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,7 +3,7 @@
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- general enhancements
-vim.keymap.set("n", "<leader>q", ":q!<cr>", { desc = "Close Window" })
-vim.keymap.set("n", "<leader>z", ":update<cr>", { desc = "Save" })
-vim.keymap.set("n", "<leader>o", ":only<cr>", { desc = "Only" })
-vim.keymap.set("n", "<leader>gs", ":Git<cr>", { desc = "Git Status" })
+vim.keymap.set("n", "<leader>q", ":silent q<cr>", { desc = "Close Window", silent = true })
+vim.keymap.set("n", "<leader>z", ":silent update<cr>", { desc = "Save", silent = true })
+vim.keymap.set("n", "<leader>o", ":only<cr>", { desc = "Only", silent = true })
+vim.keymap.set("n", "<leader>gs", ":Git<cr>", { desc = "Git Status", silent = true })
