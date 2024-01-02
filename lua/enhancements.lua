61a3509c81e0253184efb382d4ed4df8b76887d4
chris <532797+thisguychris@users.noreply.github.com>
2024-01-03 06:25:03 +0800

    format enhancements

--- lua/enhancements.lua
+++ lua/enhancements.lua
@@ -5,13 +5,14 @@ vim.opt.foldtext = "v:lua.require'lazyvim.util'.ui.foldtext()"
 -- vim.opt.foldmethod = "expr"
 -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 --
-require'lspconfig'.svelte.setup{}
+require 'lspconfig'.svelte.setup {}
 
 -- fold settings
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.wo.foldlevel = 99
 vim.wo.foldtext =
-    [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) ]]
+[[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) ]]
 vim.wo.fillchars = "fold: "
 vim.wo.foldnestmax = 3
 vim.wo.foldminlines = 1
